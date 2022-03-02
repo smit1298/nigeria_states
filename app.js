982290a06cdec3068c3e44ffa678cdc7c91a9b31
smit1298 <61621188+smit1298@users.noreply.github.com>
2022-03-02 18:16:57 +0100

    concluded
    implemented json file to display states and lga in nigeria

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const selectElement = document.querySelector("select");
-
+console.log(nigeria);
 
 for (let i = 0; i < nigeria.length; i++) {
   const optionElement = document.createElement("option");
   optionElement.textContent = nigeria[i].state.name;
   optionElement.setAttribute("value", i);
   optionElement.setAttribute("id", i);
-  selectElement.appendChild(optionElement);
+  selectElement.insertAdjacentElement("beforeend", optionElement);
+}
+
+
+
+
+
+
+selectElement.addEventListener("click", function (e) {
+  const indexOfState = e.target.value
+  const lgaElement = document.getElementById("lga")
+  injectOption (nigeria[indexOfState].state.locals, lgaElement)
+});
+
+function injectOption(data, element) {
+  element.innerHTML = ''
+  for (let i = 0; i < data.length; i++) {
+    const optionElement = document.createElement("option");
+    optionElement.textContent = data[i].name;
+    optionElement.setAttribute("value", i);
+    optionElement.setAttribute("id", i);
+    element.insertAdjacentElement("beforeend", optionElement);
+  }
 }
